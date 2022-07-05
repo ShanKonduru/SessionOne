8aae21a389935114f71310854c85d8fd8f1e9faf
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-07-05 21:18:49 +0530

    removed find element

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const {Builder, By, Key, util } = require("selenium-webdriver");
+const {Builder} = require("selenium-webdriver");
 
 async function sessionOne() {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://www.google.com");
-    await driver.findElement(By.name("q")).sendKeys("Shan Konduru", Key.RETURN);
 }
 
 sessionOne();
